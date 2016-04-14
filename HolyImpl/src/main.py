317b8b3cf66fcf985c3e34775c69be6027649206
Luvsandondov Lkhamsuren <lkhamsurenl@gmail.com>
2016-04-14 12:38:25 -0500

    main method to call holy tree

--- HolyImpl/src/main.py
+++ HolyImpl/src/main.py
@@ -1,10 +1,21 @@
 from src.model.graph import Graph
+from src.algorithms.holy_tree import move_around_face
+import src.model.grid as grid
+import sys
 
-g = Graph()
-a = g.addVertex("a")
-b = g.addVertex("b")
+#TODO(lkhamsurenl): This should be a final calling method once prototype is done.
+def get_face_vertices(graph, names):
+    vertices = []
+    # Populate all the vertices given their names.
+    for name in names:
+        vertices.append(graph.get_vertex(name))
+    return vertices
 
-a.addEdge(b, 1)
+def holy_tree():
+    sys.setrecursionlimit(10000)
+    m, n = 6, 6
+    g1 = grid.generate_2d_grid(m, n)
+    vertices = get_face_vertices(g1, [(1, 1), (0, 1), (0, 0), (1, 0)])
+    move_around_face(g1, m, n, vertices)
 
-print(a.neighbors[b])
-print(b.neighbors)
+holy_tree()
